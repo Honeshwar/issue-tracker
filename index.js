48867e5d3bdceaabbecf80c931edaded4bb552be
Honeshwar Thakur <st566654@gmail.com>
2023-03-06 21:46:48 +0530

    listen(set address of) our server(in this machine) on port 8000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();// feature of framework just call function,  it create new
 
 //express func called,  it create new express app ,that having all functionality that express framework give//put new express application inside app variable// call to run the express server,instance entire framework
 const port = 8000;//address of server of particular application/services, help in identify different services provides by same machine
-app.listen(port , (error)=>{
+app.listen(port , (error)=>{//an address set to this particular express application server
     if(error){
         console.log("error while running express server",error);
         return;
